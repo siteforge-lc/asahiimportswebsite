8e7f9b3a165f4a5509d72683a0622a0206aa8011
siteforge-lc <siteforge.lc@gmail.com>
2026-03-10 22:46:57 -0500

    Create tailwind.config.js
    This sets up your "Red Theme."

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        asahiRed: '#D92128',
+        asahiBlack: '#1A1A1A',
+        asahiCream: '#F9F6F0',
+      },
+    },
+  },
+  plugins: [],
+}
